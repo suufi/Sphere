74bd594a48700b9d9fbbeb812861ecdb356001f6
suufi <qxest@reqxest.com>
2018-01-07 16:15:55 -0500

    Add the ModerationManager.lua script

---
+++ roblox/ModerationManager.lua
@@ -0,0 +1,37 @@
+local Try = require(script.Parent.TryLibrary)
+local BaseURL = "http://server.suufi.xyz:9000";
+
+while wait(1) do
+
+    local List = {}
+
+    -- Get list of current player IDs
+    for i, Player in pairs(game.Players:GetPlayers()) do
+        table.insert(List, Player.UserId)
+    end
+
+    -- Check for banned players currently in-game
+    Try(function ()
+        return game.HttpService:JSONDecode(
+            game.HttpService:PostAsync(BaseURL + '/bans/check/users', { players = game.HttpService:JSONEncode(List), token = TOKEN })
+        )
+    end)
+
+    -- Kick returned banned players
+    :Then(function (Response)
+        for _, PlayerId in ipairs(Response) do
+            local Player = game.Players:GetPlayerByUserId(PlayerId)
+            if Player then
+               Player:Kick('You have been banned. Please contact the game owner to appeal.');
+            end
+         end
+    end)
+
+    -- Catch errors
+    :Catch(function (Error, Stack)
+        warn(':x: [ServerCheck] Failed to parse ban data',
+            '\n\nError:\n', Error,
+            '\n\nStack:\n', Stack);
+    end)
+
+end
